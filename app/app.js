c063de9ec6cffeb7c2e9e7e82faccbbc686c412c
ashleyyy <ashleyfisher@gmail.com>
2016-02-05 12:14:52 -0800

    added confirmation

--- app/app.js
+++ app/app.js
@@ -22,11 +22,21 @@ angular.module('myApp', [
       controller: 'CreatePizzaCtrl'
     })
     .otherwise({redirectTo: '/'});
-}]);
+}])
 
-// .factory("messages",function(){
-//         return {};
-// });;
+.directive('ngConfirmClick', [function(){
+    return {
+      link: function (scope, element, attr) {
+        var msg = attr.ngConfirmClick || "Are you sure?";
+        var clickAction = attr.confirmedClick;
+        element.bind('click',function (event) {
+          if ( window.confirm(msg) ) {
+            scope.$eval(clickAction)
+          }
+        });
+      }
+    };
+}]);
 
 $(function() {
 
